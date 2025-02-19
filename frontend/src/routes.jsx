dc910bd895f805822c02149b466718567b8d994c
Kefirchik99 <jermakent@gmail.com>
2025-02-19 18:20:57 +0200

    modified routing,removed duplicates

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Header from './components/Header/Header';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import ProductListPage from './product-pages/ProductListPage/ProductListPage';
 import ProductDetailPage from './product-pages/ProductDetailPage/ProductDetailPage';
 
 const AppRoutes = () => {
     return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Navigate to="/category/all" replace />} />
-                <Route path="/category/:categoryName" element={<ProductListPage />} />
-                <Route path="/product/:id" element={<ProductDetailPage />} />
-                <Route path="*" element={<h2>404 - Not Found</h2>} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Navigate to="/category/all" replace />} />
+            <Route path="/category/:categoryName" element={<ProductListPage />} />
+            <Route path="/product/:id" element={<ProductDetailPage />} />
+            <Route path="*" element={<h2>404 - Not Found</h2>} />
+        </Routes>
     );
 };
 
